be4eea5f8a064f301026ea8954047a7e6bccdabc
Kai Kewley <kaikewley@gmail.com>
2016-12-21 23:46:58 +0000

    Initial commit

---
+++ src/main/java/io/kaicode/elasticvc/api/PathUtil.java
@@ -0,0 +1,27 @@
+package io.kaicode.elasticvc.api;
+
+public class PathUtil {
+
+	public static final String SEPARATOR = "/";
+
+	public static String getParentPath(String path) {
+		path = fatten(path);
+		final int indexOf = path.lastIndexOf(SEPARATOR);
+		if (indexOf != -1) {
+			return path.substring(0, indexOf);
+		}
+		return null;
+	}
+
+	public static String flaten(String path) {
+		return path.replace("/", "_");
+	}
+
+	public static String fatten(String path) {
+		return path.replace("_", "/");
+	}
+
+	public static boolean isRoot(String path) {
+		return !fatten(path).contains(SEPARATOR);
+	}
+}
