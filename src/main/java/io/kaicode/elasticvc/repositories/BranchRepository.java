ff09a4fef2f9506a70a7d3e01cc11cb9162867d5
Kai Kewley <kaikewley@gmail.com>
2016-12-22 00:54:55 +0000

    Maven artifacts for 0.1.0-SNAPSHOT

--- src/main/java/io/kaicode/elasticvc/repositories/BranchRepository.java
+++
@@ -1,8 +0,0 @@
-package io.kaicode.elasticvc.repositories;
-
-import io.kaicode.elasticvc.domain.Branch;
-import org.springframework.data.elasticsearch.repository.ElasticsearchCrudRepository;
-
-public interface BranchRepository extends ElasticsearchCrudRepository<Branch, String> {
-
-}
